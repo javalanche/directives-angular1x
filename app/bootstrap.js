d8009e478bf8c2d1d62fceb93c55443f4b8a1a3c
Javier Fraga <javier.s.fraga@gmail.com>
2016-03-15 06:51:22 -0500

    looks like beginning of second step but not sure

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -10,10 +10,10 @@ window.onload = function(){
 	var $injector = angular.injector(modules);
 
 	var $compile = $injector.get('$compile');
-	var compositeLinkFn = $compile($rootElement);
+	var compositeLinkFn = $compile($rootElement); //climbs DOM for directives and will replace with comments
 
 	var $rootScope = $injector.get('$rootScope');
-	compositeLinkFn($rootScope);
+	compositeLinkFn($rootScope); // runs linking function now we have a view
 
-	$rootScope.$apply();
+	$rootScope.$apply(); // this updates view
 }
---
+++ app/controller.js
@@ -0,0 +1,10 @@
+//myApp.controller('Demo', function($scope){
+	//$scope.name = 'world';
+	//$scope.leak = 'none';
+//});
+
+var Demo = function($scope) {
+	$scope.name = 'world';
+	$scope.leak = 'none';
+};
+myApp.controller('Demo', Demo);
